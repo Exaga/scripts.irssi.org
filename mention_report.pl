7ff539c0f57b70a47a68003a365cf58b6db8574e
Exaga <19157861+Exaga@users.noreply.github.com>
2026-03-31 15:20:36 +0100

    Refactor mention reporting and update metadata
    This script acts as a silent recorder while you're set as away. It eliminates the need to manually scroll and search through logs or many lines of channel chat to find out why your nick was mentioned.
    
    It's specifically designed to work alongside autoaway.pl and assumes to report any missed mentions.
    
    How it works...
    
    - When you set yourself as away (or if you're using autoaway.pl) the script initialises a temporary memory cache. It ignores regular chatter but records any line where your nick is mentioned in a channel or PM. It strips the formatting and timestamps and stores it in RAM.
    
    - When you come back from being away, the script triggers and lists any messages containing your nick that were sent while you were away. Instead of having to grep logs or scroll up many pages, it outputs any cached mentions of your nick directly into the currently active window when you come back from being away.
    
    - Once the report is outputted to your screen, the cache is cleared. This means you won't see the same messages and mentions the next time you are away.

--- mention_report.pl
+++ mention_report.pl
@@ -23,61 +23,42 @@
 
 use strict;
 use Irssi;
-use POSIX qw(strftime);
+use vars qw($VERSION %IRSSI);
 
-our $VERSION = '1.0';
-our %IRSSI = (
+$VERSION = "1.01";
+%IRSSI = (
     authors     => 'Exaga',
-    contact     => 'sairpiproject@gmail.com',	
+    contact     => 'sairpiproject@gmail.com',
     name        => 'mention_report',
-    description => 'Caches nick mentions while away and reports on return',
-	license     => 'MIT'
+    description => 'caches highlights while away',
+    license     => 'Public Domain'
 );
 
-my @mentions;
-my $is_away = 0;
+my @msgs;
 
-sub catch_mention {
+sub sig_print_text {
     my ($dest, $text, $stripped) = @_;
-    # Only cache if away and the message is a Hilight or a Private Message
-    if ($is_away && ($dest->{level} & (MSGLEVEL_HILIGHT | MSGLEVEL_MSGS))) {
-        my $time = strftime("%H:%M", localtime);
-        my $target = $dest->{target} || "PM";
-        push @mentions, "[$time] $target: $stripped";
+    return unless $dest->{server}->{usermode_away};
+    
+    if ($dest->{level} & (MSGLEVEL_HILIGHT | MSGLEVEL_MSGS)) {
+        my $time = Irssi::settings_get_str('timestamp_format') || "%H:%M";
+        push @msgs, " " . CORE::localtime() . " " . ($dest->{target} || "PM") . "> $stripped";
     }
 }
 
-sub toggle_away {
-    my $server = Irssi::active_server();
-    if ($server && $server->{usermode_away}) {
-        $is_away = 1;
-    } else {
-        $is_away = 0;
-        report_mentions();
+sub event_away {
+    my ($server, $away) = @_;
+    return if $away; # only trigger when coming back
+    
+    if (@msgs) {
+        Irssi::print("--- missed mentions ---");
+        Irssi::print($_) for @msgs;
+        Irssi::print("--- end ---");
+        @msgs = ();
     }
 }
 
-sub report_mentions {
-    if (@mentions) {
-        Irssi::print("--- MENTION REPORT ---", MSGLEVEL_CLIENTCRAP);
-        foreach my $line (@mentions) {
-            Irssi::print($line, MSGLEVEL_CLIENTCRAP);
-        }
-        Irssi::print("--- END OF REPORT ---", MSGLEVEL_CLIENTCRAP);
-        @mentions = (); # Clear cache
-    }
-}
-
-Irssi::signal_add('print text', 'catch_mention');
-Irssi::signal_add('gui away enabled', sub { $is_away = 1; });
-Irssi::signal_add('gui away disabled', 'report_mentions');
-
-sub sig_unload {
-    Irssi::print("mention_report: Unloaded (cache cleared)");
-}
-
-Irssi::signal_add_first('gui unload', 'sig_unload');
-
-Irssi::print("mention_report $VERSION loaded - use /away to start caching mentions.");
+Irssi::signal_add('print text', 'sig_print_text');
+Irssi::signal_add('away mode changed', 'event_away');
 
 # EOF<*>
